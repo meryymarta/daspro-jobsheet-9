564224b3763c0cde34a5d7d6a91a50d2e9f8baad
meryymarta <meriammartadinata@gmail.com>
2024-11-16 05:27:59 +0700

    experiment 3 question 1

--- ArrayAverageScore12.java
+++ ArrayAverageScore12.java
@@ -5,18 +5,23 @@ public class ArrayAverageScore12 {
 
         //declaration variable
         int[] score = new int[10];
+        int passedStudent = 0;
         double total = 0;
         double average;
-       
+
+        //looping
         for (int i = 0; i < score.length; i++){
             System.out.print("The student score " + (i + 1) + ": ");
             score[i] = input12.nextInt();
+            if(score[i] >= 70) {
+                passedStudent++;
         }
+    }
         for (int i = 0; i < score.length; i++){
             total+= score[i];
         }
         average = total / score.length;
         System.out.println("The class average score is " + average);
-        
+        System.out.println("The number of student who passed is "+passedStudent);
     }
 }
