cdaa3f1d9f505642aa518f0e266e97436a9fdb82
meryymarta <meriammartadinata@gmail.com>
2024-11-16 04:28:21 +0700

    experiment 1 question 3

--- ArrayNumbers12.java
+++ ArrayNumbers12.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 public class ArrayNumbers12 {
     public static void main(String[] args) {
-        double[] num = new double[4];
-        num[0] = 5.0;
-        num[1] = 12876;
-        num[2] = 7.5;
-        num[3] = 200000;
+        int[] num = new int[4];
+        num[0] = 5;
+        num[1] = 12;
+        num[2] = 7;
+        num[3] = 20;
 
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
+        for(int i = 0; i < 4; i++) {
+            System.out.println(num[i]);
         }
     }
+}
